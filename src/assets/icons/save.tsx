110f94a10adfd56f4fda8799a8f60dc28ea09e38
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2025-01-31 20:21:56 -0300

    fix: save icon

--- src/assets/icons/save.tsx
+++ src/assets/icons/save.tsx
@@ -4,7 +4,7 @@ export const Save = () => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="white"
+      stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
       width={24}
